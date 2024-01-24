806a91826a0395b5bbe09833197bb88a3b51544b
JohnFrancis <51811442+cpt-John@users.noreply.github.com>
2024-01-24 18:10:23 +0000

    Revert "Update home.component.ts"
    This reverts commit e7ad63f6cd72bedef2da4afcc840d4bda7cc3fde.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
   userData = {};
   data = [];
   filteredData = [];
-  baseSUrl = '';
+  baseSUrl = DataService.getBaseUrl();
   constructor(
     private fb: FormBuilder,
     private _router: Router,
@@ -64,7 +64,6 @@ export class HomeComponent implements OnInit {
   setData() {
     let jwt = window.localStorage.getItem('loginToken');
     this.requesting = true;
-    this.baseSUrl = this.service.getBaseUrl();
     let bodyData = {
       jwt,
       name: this.profileForm.get('name').value,
